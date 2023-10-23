fe178dd5d638887afc2dcb646b1ff792d96d1584
Acer <a-krickiy@mail.ru>
2023-10-23 22:57:12 +0300

    2.1. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class EvenNumberFile {
+
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("data/even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                boolean rsl = Integer.parseInt(line) % 2 == 0;
+                System.out.printf("Число %s является четным - %s%n", line, rsl);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
