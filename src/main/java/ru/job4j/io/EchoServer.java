3ca66475da8314bafdd2e33b0c65ba8bdc71a747
Acer <a-krickiy@mail.ru>
2023-11-24 01:11:16 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -14,8 +14,15 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String str = in.readLine();
-                    if (str.contains("msg=Bye")) {
+                    if (str.contains("msg=Hello")) {
+                        out.write("Hello\r\n".getBytes());
+                        System.out.println(str);
+                    } else if (str.contains("msg=Exit")) {
                         server.close();
+                        out.write("Server is complete\r\n".getBytes());
+                        System.out.println(str);
+                    } else {
+                        out.write("What?\r\n".getBytes());
                         System.out.println(str);
                     }
                     out.flush();
