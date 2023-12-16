3fc49bc8b9713a0be6d8f5b01cd59f74aade94a7
Acer <a-krickiy@mail.ru>
2023-12-16 19:22:23 +0300

    0. Что такое база данных. [#6860 #466796]

--- db/create.sql
+++ db/create.sql
@@ -1,1 +1,12 @@
+create table christmas_tree(
+	id serial primary key,
+	price money,
+    live bool,
+    height decimal(1,1)
+);
+insert into christmas_tree(price, live, height) values(5000, 'true', 0.9);
+select * from christmas_tree;
+update christmas_tree set price = 3000;
+select * from christmas_tree;
+delete from christmas_tree;
 select * from christmas_tree;
