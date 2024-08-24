d08ebc677d76a30bbe9f4c7bc0ad483749678b3b
Acer <a-krickiy@mail.ru>
2024-08-24 14:52:44 +0300

    Обход дерева в глубину [#504974]

--- src/main/java/ru/job4j/algo/newcoll/tree/TreeUtils.java
+++ src/main/java/ru/job4j/algo/newcoll/tree/TreeUtils.java
@@ -1,9 +1,11 @@
 package ru.job4j.algo.newcoll.tree;
 
 import ru.job4j.collection.SimpleQueue;
+import ru.job4j.collection.SimpleStack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class TreeUtils<T> {
 
@@ -54,5 +56,96 @@ public class TreeUtils<T> {
             list.add(node.getValue());
         }
         return list;
+    }
+
+    /**
+     * Метод обходит дерево root и добавляет к узлу с ключом parent
+     * новый узел с ключом child, при этом на момент добавления узел с ключом parent
+     * уже должен существовать в дереве, а узла с ключом child в дереве быть не должно
+     *
+     * @param root   корень дерева
+     * @param parent ключ узла-родителя
+     * @param child  ключ узла-потомка
+     * @return true если добавление произошло успешно и false в обратном случае.
+     * @throws IllegalArgumentException если root является null
+     */
+    public boolean add(Node<T> root, T parent, T child) {
+        boolean isAdd;
+        Optional<Node<T>> optional = findByKey(root, parent);
+        if (optional.isEmpty() || findByKey(root, child).isPresent()) {
+            isAdd = false;
+        } else {
+            isAdd = optional.get().getChildren().add(new Node<>(child));
+        }
+        return isAdd;
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key
+     *
+     * @param root корень дерева
+     * @param key  ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> findByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException("Queue is empty");
+        }
+        SimpleStack<Node<T>> stack = new SimpleStack<>();
+        stack.push(root);
+        Node<T> node = null;
+        while (!stack.isEmpty()) {
+            Node<T> tNode = stack.pop();
+            if (tNode.getValue().equals(key)) {
+                node = tNode;
+                break;
+            }
+            List<Node<T>> children = tNode.getChildren();
+            if (!children.isEmpty()) {
+                children.forEach(stack::push);
+            }
+        }
+        return Optional.ofNullable(node);
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key,
+     * при этом из дерева root удаляется все поддерево найденного узла
+     *
+     * @param root корень дерева
+     * @param key  ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> divideByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException("Queue is empty");
+        }
+        SimpleStack<Node<T>> stack = new SimpleStack<>();
+        List<Node<T>> list = new ArrayList<>();
+        stack.push(root);
+        Node<T> node = null;
+        while (!stack.isEmpty()) {
+            Node<T> tNode = stack.pop();
+            list.add(tNode);
+            if (tNode.getValue().equals(key)) {
+                node = tNode;
+                if (list.size() > 1) {
+                    for (Node<T> tNode1 : list) {
+                        List<Node<T>> children = tNode1.getChildren();
+                        if (children.remove(node)) {
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            List<Node<T>> children = tNode.getChildren();
+            if (!children.isEmpty()) {
+                children.forEach(stack::push);
+            }
+        }
+        return Optional.ofNullable(node);
     }
 }
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -2,13 +2,21 @@ package ru.job4j.collection;
 
 public class SimpleStack<T> {
 
-    private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private final ForwardLinked<T> linked = new ForwardLinked<>();
+    private int size = 0;
 
     public T pop() {
-        return linked.deleteFirst();
+        T element = linked.deleteFirst();
+        size--;
+        return element;
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
