c1821201b1d0373bd653c58371649a02b5270aa8
Acer <a-krickiy@mail.ru>
2024-08-28 00:37:20 +0300

    Бинарное дерево поиска. Методы добавления, поиска и обхода дерева. [#504983]

---
+++ src/main/java/ru/job4j/algo/collection/binarytree/BinarySearchTree.java
@@ -0,0 +1,144 @@
+package ru.job4j.algo.collection.binarytree;
+
+import java.util.*;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    private Node root;
+
+    public boolean put(T key) {
+        boolean result;
+        if (Objects.isNull(root)) {
+            root = new Node(key);
+            result = true;
+        } else {
+            result = put(root, key);
+        }
+        return result;
+    }
+
+    private boolean put(Node node, T key) {
+        if (key.compareTo(node.key) < 0) {
+            if (Objects.isNull(node.left)) {
+                node.left = new Node(key);
+                return true;
+            } else {
+                return put(node.left, key);
+            }
+        } else if (key.compareTo(node.key) > 0) {
+            if (Objects.isNull(node.right)) {
+                node.right = new Node(key);
+                return true;
+            } else {
+                return put(node.right, key);
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean contains(T key) {
+        return Objects.nonNull(root) && Objects.nonNull(find(root, key));
+    }
+
+    private Node find(Node node, T key) {
+        if (Objects.isNull(node) || key.compareTo(node.key) == 0) {
+            return node;
+        } else if (key.compareTo(node.key) < 0) {
+            return find(node.left, key);
+        } else {
+            return find(node.right, key);
+        }
+    }
+
+    public List<T> inSymmetricalOrder() {
+        List<T> result = new ArrayList<>();
+        Node node = root;
+        return inSymmetricalOrder(node, result);
+    }
+
+    private List<T> inSymmetricalOrder(Node localRoot, List<T> list) {
+        if (Objects.nonNull(localRoot)) {
+            inSymmetricalOrder(localRoot.left, list);
+            list.add(localRoot.key);
+            inSymmetricalOrder(localRoot.right, list);
+        }
+        return list;
+    }
+
+    public List<T> inPreOrder() {
+        List<T> result = new ArrayList<>();
+        Node node = root;
+        return inPreOrder(node, result);
+    }
+
+    private List<T> inPreOrder(Node localRoot, List<T> list) {
+        if (Objects.nonNull(localRoot)) {
+            list.add(localRoot.key);
+            inPreOrder(localRoot.left, list);
+            inPreOrder(localRoot.right, list);
+        }
+        return list;
+    }
+
+    public List<T> inPostOrder() {
+        List<T> result = new ArrayList<>();
+        Node node = root;
+        return inPostOrder(node, result);
+    }
+
+    private List<T> inPostOrder(Node localRoot, List<T> list) {
+        if (Objects.nonNull(localRoot)) {
+            inPostOrder(localRoot.left, list);
+            inPostOrder(localRoot.right, list);
+            list.add(localRoot.key);
+        }
+        return list;
+    }
+
+    public T minimum() {
+        return Objects.nonNull(root) ? minimum(root).key : null;
+    }
+
+    private Node minimum(Node node) {
+        return Objects.isNull(node.left) ? node : minimum(node.left);
+    }
+
+    public T maximum() {
+        return Objects.nonNull(root) ? maximum(root).key : null;
+    }
+
+    private Node maximum(Node node) {
+        return Objects.isNull(node.right) ? node : maximum(node.right);
+    }
+
+    @Override
+    public String toString() {
+        return PrintTree.getTreeDisplay(root);
+    }
+
+    private class Node implements VisualNode {
+        private final T key;
+        private Node left;
+        private Node right;
+
+        public Node(T key) {
+            this.key = key;
+        }
+
+        @Override
+        public VisualNode getLeft() {
+            return left;
+        }
+
+        @Override
+        public VisualNode getRight() {
+            return right;
+        }
+
+        @Override
+        public String getText() {
+            return key.toString();
+        }
+    }
+}
